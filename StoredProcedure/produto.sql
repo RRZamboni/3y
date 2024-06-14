dc827e46d65a1b72dbb96e296f2a10c0ead9e580
RRZamboni <rafael.zamboni@etip.santoandre.br>
2024-06-14 09:16:48 -0300

    Add files via upload

--- StoredProcedure/produto.sql
+++ StoredProcedure/produto.sql
@@ -1,12 +1,55 @@
-CREATE DATABASE produto
+-- phpMyAdmin SQL Dump
+-- version 4.7.9
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: 25-Out-2018 às 12:50
+-- Versão do servidor: 5.7.21
+-- PHP Version: 5.6.35
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `produto`
+--
+
+DELIMITER $$
+--
+-- Procedures
+--
+DROP PROCEDURE IF EXISTS `todosRegistros`$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `todosRegistros` ()  BEGIN
+ 	SELECT * FROM produto;
+ END$$
+
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `produto`
+--
+
+DROP TABLE IF EXISTS `produto`;
 CREATE TABLE IF NOT EXISTS `produto` (
   `codigo` int(11) DEFAULT NULL,
   `descricao` varchar(100) DEFAULT NULL,
   `estoque` int(11) DEFAULT NULL,
-  `preco` float DEFAULT NULL)
+  `preco` float DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
+--
+-- Extraindo dados da tabela `produto`
+--
 
 INSERT INTO `produto` (`codigo`, `descricao`, `estoque`, `preco`) VALUES
 (1, 'Sapato Preto', 6, 180),
@@ -29,3 +72,8 @@ INSERT INTO `produto` (`codigo`, `descricao`, `estoque`, `preco`) VALUES
 (18, 'Travesseiro Viscoelástico', 10, 60),
 (19, 'Travesseiro Espuma', 20, 22),
 (20, 'Travesseiro Pluma', 6, 40);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
